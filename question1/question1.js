edbfd13f63fde9a87829189edbef860662e44b07
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-10-05 12:19:21 -0400

    updated question 1 lowerCaseWords function with promise returns

--- question1/question1.js
+++ question1/question1.js
@@ -1,7 +1,18 @@
 const mixedArray = ['PIZZA', 10, true, 25, false, 'Wings']
-const lowerCaseWords = (arrays) => arrays.filter(arr =>
-    (typeof arr === "string" && !Number(arr)))
-    .map(arr => arr.toLowerCase())
+const lowerCaseWords = (arrays) => new Promise((response, reject) => {
+    let filterArray = arrays.filter(arr =>
+        (typeof arr === "string" && !Number(arr)))
+        .map(arr => arr.toLowerCase())
+
+    if (filterArray.length >= 1) {
+        return response(filterArray)
+    } else if (filterArray.length === 0) {
+        return reject('no string value found to modify on the array')
+    }
+})
 
 
-console.log(lowerCaseWords(mixedArray))
+lowerCaseWords(mixedArray).then(message => {
+    console.log(message)
+}).catch((err) => console.log(err))
+
